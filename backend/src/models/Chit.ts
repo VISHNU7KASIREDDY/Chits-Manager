800d3cc73292621d52fd76c53c9995637e759f18
VISHNU7KASIREDDY <vishnukasireddy28@gmail.com>
2026-02-11 22:19:53 +0530

    chit model

---
+++ backend/src/models/Chit.ts
@@ -0,0 +1,76 @@
+import mongoose, { Schema } from "mongoose"
+import {
+  IChit,
+  IMonth,
+  IMonthlyPayment,
+} from "../utils/interfaces/chit.interface"
+
+const monthlyPaymentSchema = new Schema<IMonthlyPayment>({
+  member: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  isPaid: {
+    type: Boolean,
+    default: false,
+  },
+  paidDate: Date,
+})
+
+const monthSchema = new Schema<IMonth>({
+  monthNumber: { type: Number, required: true },
+  auctionAmount: Number,
+  winner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  bonusPerMember: Number,
+  payments: [monthlyPaymentSchema],
+})
+
+const chitSchema = new Schema<IChit>(
+  {
+    name: { type: String, required: true },
+
+    chitValue: { type: Number, required: true },
+    monthlyAmount: { type: Number, required: true },
+    totalMembers: { type: Number, required: true },
+    duration: { type: Number, required: true },
+
+    startDate: {
+      type: Date,
+      required: true,
+      index: true,
+    },
+
+    endDate: {
+      type: Date,
+      required: true,
+      index: true,
+    },
+
+    members: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    months: [monthSchema],
+
+    status: {
+      type: String,
+      enum: ["active", "completed"],
+      default: "active",
+    },
+  },
+  { timestamps: true }
+)
+
+
+chitSchema.index({ members: 1 })
+chitSchema.index({ startDate: 1 })
+chitSchema.index({ endDate: 1 })
+
+export const Chit = mongoose.model<IChit>("Chit", chitSchema)
